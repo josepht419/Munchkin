3fbbac2ca86b4a255e9d2b82c070b6207b52e93a
Brian Sanchez <brianxschez@gmail.com>
2021-07-26 13:58:29 -0400

    Update13

--- Munchkin FOLDER/backend/handlers/CurseCardHandler.java
+++ Munchkin FOLDER/backend/handlers/CurseCardHandler.java
@@ -7,8 +7,4 @@ import treasure_cards.*;
 public class CurseCardHandler {
     // IMPORTANT NOTE: Handle specific curses in their own separate methods as needed, as opposed to coding everything in a switch statement.
     // This will be better for debugging and overall design.
-    public static void handleCurse(Player target, Card curse) {
-
-    }
-
 }
